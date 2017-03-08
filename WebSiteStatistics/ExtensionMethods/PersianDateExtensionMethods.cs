49140c7ced776f3f9e78b516f7b42170719808c1
CSSPRO <CSSPRO@DESKTOP-M2FB5S1>
2017-03-08 16:15:27 +0330

    added PersianDateExtensionMethods

---
+++ WebSiteStatistics/ExtensionMethods/PersianDateExtensionMethods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace System
+{
+    public static class PersianDateExtensionMethods
+    {
+        private static CultureInfo _Culture;
+        public static CultureInfo GetPersianCulture()
+        {
+            if (_Culture == null)
+            {
+                _Culture = new CultureInfo("fa-IR");
+                DateTimeFormatInfo formatInfo = _Culture.DateTimeFormat;
+                formatInfo.AbbreviatedDayNames = new[] { "ی", "د", "س", "چ", "پ", "ج", "ش" };
+                formatInfo.DayNames = new[] { "یکشنبه", "دوشنبه", "سه شنبه", "چهار شنبه", "پنجشنبه", "جمعه", "شنبه" };
+                var monthNames = new[]
+                {
+                    "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن",
+                    "اسفند",
+                    ""
+                };
+                formatInfo.AbbreviatedMonthNames =
+                    formatInfo.MonthNames =
+                    formatInfo.MonthGenitiveNames = formatInfo.AbbreviatedMonthGenitiveNames = monthNames;
+                formatInfo.AMDesignator = "ق.ظ";
+                formatInfo.PMDesignator = "ب.ظ";
+                formatInfo.ShortDatePattern = "yyyy/MM/dd";
+                formatInfo.LongDatePattern = "dddd, dd MMMM,yyyy";
+                formatInfo.FirstDayOfWeek = DayOfWeek.Saturday;
+                System.Globalization.Calendar cal = new PersianCalendar();
+
+                FieldInfo fieldInfo = _Culture.GetType().GetField("calendar", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (fieldInfo != null)
+                    fieldInfo.SetValue(_Culture, cal);
+
+                FieldInfo info = formatInfo.GetType().GetField("calendar", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (info != null)
+                    info.SetValue(formatInfo, cal);
+
+                _Culture.NumberFormat.NumberDecimalSeparator = "/";
+                _Culture.NumberFormat.DigitSubstitution = DigitShapes.NativeNational;
+                _Culture.NumberFormat.NumberNegativePattern = 0;
+            }
+            return _Culture;
+        }
+
+        public static string ToPeString(this DateTime date, string format = "yyyy/MM/dd")
+        {
+            return date.ToString(format, GetPersianCulture());
+        }
+    }
+}
